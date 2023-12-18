0297fddcf770cf41f06ea53a2a0a45ece5f6edaa
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-12-18 16:14:30 +0300

    feat : register taxonomies

--- coders.php
+++ coders.php
@@ -10,6 +10,8 @@ require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 use Coders\Coders_CPT;
 use Coders\Coders_RESTAPI;
+use Coders\Coders_TAXONOMIES;
 
 Coders_CPT::register();
 Coders_RESTAPI::register();
+Coders_TAXONOMIES::register();
---
+++ src/Coders_TAXONOMIES.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Coders;
+
+class Coders_TAXONOMIES
+{
+    public static function register()
+    {
+        add_action('after_setup_theme', [self::class, 'register_taxonomies_team']);
+        add_action('after_setup_theme', [self::class, 'register_taxonomies_work']);
+    }
+    public static function register_taxonomies_team()
+    {
+        register_taxonomy(
+            'team-post', // The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces).
+            'members', // post type name
+            [
+                'hierarchical' => true,
+                'label' => 'Poste de la personne', // display name
+            ]
+        );
+    }
+
+    public static function register_taxonomies_work()
+    {
+        register_taxonomy(
+            'work-categories', // The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces).
+            'works', // post type name
+            [
+                'hierarchical' => true,
+                'label' => 'Classification du projet', // display name
+            ]
+        );
+    }
+}
